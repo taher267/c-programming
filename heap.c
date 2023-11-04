93e6f5033a0876c776f749fa2048341343a7ad7b
ABU TAHER <abutaher267@gmail.com>
2023-11-04 11:47:12 +0600

    basic heap

--- heap.c
+++ heap.c
@@ -7,6 +7,20 @@ int main()
     points = (int *)malloc(5 * sizeof(int));
     free(points);
     // printf("%d\n", points);
+    int i, howMany, total;
+    float average = 0.0;
+    int *printsArray;
+    printf("How manuy numbers do you want to average?\n");
+    scanf("%d", &howMany);
+    printsArray = (int *)malloc(howMany * sizeof(int));
+    printf("Enter them!\n");
+    for (i = 0; i < howMany; i++)
+    {
+        scanf(" %d", &printsArray[i]);
+        total += printsArray[i];
+    }
+    average = (float)total / (float)howMany;
+    printf("Average is %f", average);
 
     return 0;
 }
